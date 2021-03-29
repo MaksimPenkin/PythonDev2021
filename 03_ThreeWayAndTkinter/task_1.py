46dbbd47492aa4117762bb2009f5db46f3c6a839
MaksimPenkin <penkin97@gmail.com>
2021-03-29 17:19:36 +0300

    new and quit buttons added

--- 03_ThreeWayAndTkinter/task_1.py
+++ 03_ThreeWayAndTkinter/task_1.py
@@ -17,11 +17,28 @@ class Application(tk.Frame):
         root=self.winfo_toplevel()
         root.rowconfigure(0, weight=1)
         root.columnconfigure(0, weight=1)
-        for i in range(self.h):
+        for i in range(self.h+1):
             self.rowconfigure(i, weight=1)
         for j in range(self.w):
             self.columnconfigure(j, weight=1)
         
+        # New button
+        self.button_new = tk.Button(master=self,
+                                    text='New',
+                                    activebackground='green yellow',
+                                    bg='lime green',
+                                    command=None)
+        self.button_new.grid(row=0, column=0, columnspan=2, sticky='NS')
+
+        # Quit button
+        self.button_quit = tk.Button(master=self,
+                                    text='Quit',
+                                    activebackground='honeydew3',
+                                    bg='honeydew4',
+                                    command=None)
+        self.button_quit.grid(row=0, column=2, columnspan=2, sticky='NS')
+
+        # Number buttons
         for i in range(self.h):
             for j in range(self.w):
                 self.button_number[i][j] = tk.Button(master=self,
@@ -29,7 +46,7 @@ class Application(tk.Frame):
                                                     activebackground='gold',
                                                     bg='goldenrod',
                                                     command=None)
-                self.button_number[i][j].grid(row=i, column=j, sticky='NSEW')
+                self.button_number[i][j].grid(row=i+1, column=j, sticky='NSEW')
 
 app = Application()
 app.master.title('Task 1 application')
