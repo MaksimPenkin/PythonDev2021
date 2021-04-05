6c1188648b7f7439d606e8ec71b132d3767764ed
MaksimPenkin <penkin97@gmail.com>
2021-04-05 22:08:01 +0300

    circle adding on left mouse click and clearing canvas option aded

--- 05_SshAndSmartWidgents/run.py
+++ 05_SshAndSmartWidgents/run.py
@@ -4,13 +4,30 @@
 
 import tkinter as tk
 
+class Oval:
+    def __init__(self, x_center, y_center, r):
+        self.x_center = x_center
+        self.y_center = y_center 
+        self.r = r
+
+        self.x0 = self.x_center - self.r
+        self.y0 = self.y_center - self.r 
+
+        self.x1 = self.x_center + self.r
+        self.y1 = self.y_center + self.r
+
 class CustomText(tk.Text):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs) 
 
+    def clear(self):
+        pass
+
 class CustomCanvas(tk.Frame):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs) 
+        self.ovals = []
+
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
         for i in range(2):
@@ -32,13 +49,20 @@ class CustomCanvas(tk.Frame):
         self.canvas.bind("<Motion>", self.on_mouse_move)
 
     def on_mouse_left_push(self, e):
-        print(e.x, e.y)
+        oval = Oval(e.x, e.y, 10)
+        self.ovals.append(self.canvas.create_oval(oval.x0, oval.y0, oval.x1, oval.y1))
 
     def on_mouse_left_release(self, e):
-        print('RELEASED') 
+        # print('RELEASED') 
+        pass
 
     def on_mouse_move(self, e):
         self.cursor_info_label.config(text='x: {}; y: {}'.format(e.x, e.y))
+
+    def clear(self):
+        self.canvas.delete('all')
+        self.ovals = []
+        self.cursor_info_label.config(text='x: 0; y: 0')
 
 class Application(tk.Frame):
     """
@@ -67,19 +91,26 @@ class App(Application):
         self.quit_Button = tk.Button(self, text='Quit', command=self.master.quit)
         self.update_txt_Button = tk.Button(self, text='Update TXT', command=None)
         self.update_canvas_Button = tk.Button(self, text='Update Canvas', command=None)
+        self.clear_Button = tk.Button(self, text='Clear', command=self.clear)
 
         self.label1 = tk.Label(self, text='TXT field', background='SkyBlue1')
         self.label2 = tk.Label(self, text='Canvas field', background='pink1')
+        
         self.text = CustomText(self, undo=True, font="fixed", inactiveselectbackground="MidnightBlue")
         self.canvas = CustomCanvas(self)
 
         self.quit_Button.grid(row=0, column=0, sticky='NSEW')
+        self.clear_Button.grid(row=0, column=1, sticky='NSEW')
         self.update_txt_Button.grid(row=1, column=0, sticky='NSEW')
         self.update_canvas_Button.grid(row=1, column=1, sticky='NSEW')
         self.label1.grid(row=2, column=0, sticky='NSEW')
         self.label2.grid(row=2, column=1, sticky='NSEW')
         self.text.grid(row=3, column=0, sticky='NSEW')
         self.canvas.grid(row=3, column=1, sticky='NSEW')
+
+    def clear(self):
+        self.text.clear()
+        self.canvas.clear()
 
 if __name__ == "__main__":
     app = App()
