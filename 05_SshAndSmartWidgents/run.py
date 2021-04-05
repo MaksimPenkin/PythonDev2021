7b63c455f67a5c4b79af6aaca1cdcff57a736cc6
MaksimPenkin <penkin97@gmail.com>
2021-04-05 21:57:15 +0300

    mouse coords tracking on canvas

--- 05_SshAndSmartWidgents/run.py
+++ 05_SshAndSmartWidgents/run.py
@@ -8,9 +8,37 @@ class CustomText(tk.Text):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs) 
 
-class CustomCanvas(tk.Canvas):
+class CustomCanvas(tk.Frame):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs) 
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+        for i in range(2):
+            self.rowconfigure(i, weight=1)
+        for j in range(1):
+            self.columnconfigure(j, weight=1)
+        self.grid(sticky="NEWS")
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.canvas = tk.Canvas(self)
+        self.cursor_info_label = tk.Label(self, text='x: 0; y: 0', background='gray79')
+
+        self.cursor_info_label.grid(row=0, column=0, sticky="NEWS")
+        self.canvas.grid(row=1, column=0, sticky="NEWS") 
+
+        self.canvas.bind("<Button-1>", self.on_mouse_left_push)
+        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_left_release)
+        self.canvas.bind("<Motion>", self.on_mouse_move)
+
+    def on_mouse_left_push(self, e):
+        print(e.x, e.y)
+
+    def on_mouse_left_release(self, e):
+        print('RELEASED') 
+
+    def on_mouse_move(self, e):
+        self.cursor_info_label.config(text='x: {}; y: {}'.format(e.x, e.y))
 
 class Application(tk.Frame):
     """
@@ -57,3 +85,5 @@ if __name__ == "__main__":
     app = App()
     app.master.title('05_SshAndSmartWidgents application')
     app.mainloop()
+
+
